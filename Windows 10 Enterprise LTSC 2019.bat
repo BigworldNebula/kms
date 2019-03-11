a10cb410335581b045627fc03cbe8124b3d154f5
it 狗 <48358783+kwokaa2019@users.noreply.github.com>
2019-03-11 21:10:05 +0800

    Add files via upload

---
+++ Windows 10 Enterprise LTSC 2019.bat
@@ -0,0 +1,47 @@
+@echo off
+
+rem 獲取管理員權限
+:: BatchGotAdmin  
+:-------------------------------------  
+rem  --> Check for permissions  
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+rem --> If error flag set, we do not have admin.  
+if '%errorlevel%' NEQ '0' (  
+    echo 獲取管理員權限中,如果UAC彈窗,請選擇允許...
+    goto UACPrompt  
+) else ( goto gotAdmin )   
+:UACPrompt  
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"  
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"   
+    "%temp%\getadmin.vbs"
+    exit /B  
+:gotAdmin  
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )  
+    pushd "%CD%"  
+CD /D "%~dp0"
+
+
+
+echo 本程式將執行Windows 10 Enterprise LTSC 2019版本的啟動認證的設定
+
+echo 請確認您執行本程式時是否已經選擇以*系統管理者身分執行*
+
+echo 設定Windows 10 Pro 序號
+
+cscript "%SystemRoot%\system32\slmgr.vbs" /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D
+
+echo 設定 KMS 金鑰管理伺服器
+
+cscript "%SystemRoot%\system32\slmgr.vbs" /skms 192.168.0.1:1688
+
+echo 啟動Windows 10 Enterprise LTSC 2019
+
+cscript "%SystemRoot%\system32\slmgr.vbs" /ato
+
+echo 顯示Microsoft Windows 授權資訊
+
+cscript "%SystemRoot%\system32\slmgr.vbs" /dlv
+
+echo 完成大量啟用程序
+
+pause
